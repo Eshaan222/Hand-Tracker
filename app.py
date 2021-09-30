4e66b8301f8f9e0bd13f9c33141410914f1438d9
Eshaan <eshaangupta.in@gmail.com>
2021-09-30 12:06:26 +0530

    commit

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False)
